1550cbfcf58e44a4c9770a56c6c64e8e916388cf
Walter Smith <wattrlz@hotmail.com>
2023-07-11 21:51:46 -0400

    Initial Commit

---
+++ 20230711-WalterSmith-NYCSchools/_0230711_WalterSmith_NYCSchoolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  _0230711_WalterSmith_NYCSchoolsApp.swift
+//  20230711-WalterSmith-NYCSchools
+//
+//  Created by Walter Smith on 7/11/23.
+//
+
+import SwiftUI
+
+@main
+struct _0230711_WalterSmith_NYCSchoolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
