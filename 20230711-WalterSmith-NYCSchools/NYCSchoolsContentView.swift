bc089d5ed99f482a4d2e6f231ef536ed94bd92e4
Walter Smith <wattrlz@hotmail.com>
2023-07-12 11:05:55 -0400

    this should get some laughs

--- 20230711-WalterSmith-NYCSchools/NYCSchoolsContentView.swift
+++ 20230711-WalterSmith-NYCSchools/NYCSchoolsContentView.swift
@@ -10,23 +10,28 @@ import SwiftUI
 struct NYCSchoolsContentView: View {
     @ObservedObject var delegate = NYCSchoolsContentViewDelegate()
     var body: some View {
-        NavigationView {
+        NavigationStack {
             VStack {
-                Text("Schools").font(.title2).foregroundColor(.blue)
-                Text("(click for details)").font(.caption2).foregroundColor(.blue)
-                List(delegate.schools) { school in
-                    VStack {
-                        NavigationLink( school.school_name,
-                                        destination: NYCSchoolDetailView(delegate,school)
-                        )
-                    }
-                    .padding()
+                Text("NYC Schools")
+                Text("(click for details")
+                Table(delegate.schools) {
+                    //TableColumn("school_name", value: \.school_name)
+                    TableColumn("Schools", content: {NYCSchoolsRow(school: $0,delegate: self)})
                 }
             }
         }
     }
 }
 
+struct NYCSchoolsRow: View {
+    var school : School
+    var delegate : any View
+    var body: some View {
+        Text(school.school_name).onTapGesture {
+            //load NYCSchoolDetailView
+        }
+    }
+}
 protocol NYCSchoolDetailViewDelegate {
     var details : SchoolDetails {get}
     func getDetails(forSchool:String)
